9492292a9054b572054bbc604e3e0c9c7e0b2548
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-17 17:45:38 +0530

    Update OTP.js

--- OTP.js
+++ OTP.js
@@ -2,8 +2,9 @@
 let otp1 = ''
 let length = 4
 for(let i = 0; i < length; i++){
-    let otp = (Math.random() *10)+1
+    let otp = Math.random() * 10
     otp = Math.floor(otp)
+    otp += 1;
     otp = otp.toString()
     otp1 += otp;
 }
