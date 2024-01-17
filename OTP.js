0849f7a8f85a08c44df83102201d936adfe4a660
manikant8 <102610169+manikant8@users.noreply.github.com>
2024-01-17 13:03:27 +0530

    Update OTP.js

--- OTP.js
+++ OTP.js
@@ -1,4 +1,4 @@
-// Numeric password generator
+// Numeric OTP generator
 let otp1 = ''
 let length = 4
 for(let i = 0; i < length; i++){
@@ -9,3 +9,10 @@ for(let i = 0; i < length; i++){
     otp1 += otp;
 }
 console.log(otp1);
+
+//using math.floor()
+let length = 6
+let otp = Math.random()
+console.log(otp);
+let x = Math.pow(10, length)
+console.log(otp.toFixed(length) * x);
